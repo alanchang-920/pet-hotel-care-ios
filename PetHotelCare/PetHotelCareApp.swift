e949bd8564f902652cc47189ca9cf1bc34c61004
alanchang-920 <alanchang0920@gmail.com>
2026-09-02 20:10:28 +1000

    Initial Commit

---
+++ PetHotelCare/PetHotelCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetHotelCareApp.swift
+//  PetHotelCare
+//
+//  Created by Chang Chia ming on 2026/9/2.
+//
+
+import SwiftUI
+
+@main
+struct PetHotelCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
